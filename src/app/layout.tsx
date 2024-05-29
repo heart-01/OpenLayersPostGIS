4f20bc2135191dba80c8293b080241bf274e2a1c
siwat jermwatthana <siwat.j@hiveground.com>
2024-05-29 15:14:12 +0700

    Update CSS styles and add save button to MapWrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "../../node_modules/ol/ol.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/MapWrapper.tsx
+++ src/components/MapWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
@@ -15,6 +15,7 @@ import axios, { AxiosRequestConfig } from "axios";
 import Style from "ol/style/Style";
 import Stroke from "ol/style/Stroke";
 import Fill from "ol/style/Fill";
+import Modify from "ol/interaction/Modify";
 
 const KEY_RASTER_LAYER = "OfUznJ15NTmFXjk0lB4s";
 const URL_GEOSERVER = process.env.NEXT_PUBLIC_GEOSERVER_URL as string;
@@ -26,7 +27,7 @@ const getFeatures = async () => {
       service: "WFS",
       version: "1.0.0",
       request: "GetFeature",
-      typeName: "	test:test",
+      typeName: "test:test",
       maxFeatures: 50,
       outputFormat: "application/json",
     },
@@ -36,7 +37,7 @@ const getFeatures = async () => {
   };
 
   try {
-    const response = await axios.get(URL_GEOSERVER, config);
+    const response = await axios.get(`${URL_GEOSERVER}/test/ows`, config);
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -45,6 +46,8 @@ const getFeatures = async () => {
 };
 
 const MapPage = () => {
+  const [modifiedFeatures, setModifiedFeatures] = useState<Feature<Geometry>[]>([]);
+
   useEffect(() => {
     // Create map view
     const view = new View({
@@ -68,7 +71,8 @@ const MapPage = () => {
       loader: async (extent, resolution, projection) => {
         try {
           const data = await getFeatures();
-          const features = vectorSource?.getFormat()?.readFeatures(data, { dataProjection: "EPSG:4326", featureProjection: "EPSG:3857" });
+          const features = new GeoJSON()?.readFeatures(data, { dataProjection: "EPSG:4326", featureProjection: "EPSG:3857" });
+          console.log("🚀 ~ loader: ~ features:", features);
           if (features) {
             vectorSource.addFeatures(features as Feature<Geometry>[]);
           }
@@ -98,9 +102,88 @@ const MapPage = () => {
       target: "map",
       view: view,
     });
+
+    // Add modify interaction
+    const modify = new Modify({ source: vectorSource });
+    map.addInteraction(modify);
+
+    modify.on("modifyend", (event) => {
+      setModifiedFeatures(event.features.getArray().slice());
+    });
   }, []);
 
-  return <div id="map" style={{ width: "100%", height: "100vh" }}></div>;
+  const handleOnClickSave = async () => {
+    if (modifiedFeatures.length === 0) {
+      alert("No features modified.");
+      return;
+    }
+
+    // const geojson = new GeoJSON().writeFeaturesObject(modifiedFeatures, {
+    //   dataProjection: "EPSG:4326",
+    //   featureProjection: "EPSG:3857",
+    // });
+
+    // console.log(geojson)
+
+    const wfsTransaction = `<Transaction service="WFS" version="1.1.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:wfs="http://www.opengis.net/wfs"
+      xmlns:gml="http://www.opengis.net/gml"
+      xmlns:ogc="http://www.opengis.net/ogc"
+      xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd">
+      <Update typeName="test:test">
+          <Property>
+              <Name>plot_code</Name>
+              <Value>asia</Value>
+          </Property>
+          <Property>
+              <Value>
+                  <gml:Polygon srsName="EPSG:4326">
+                      <gml:exterior>
+                          <gml:LinearRing>
+                              <gml:posList>92.5977 26.09690000000002 94.7446 13.215199999999996 102.5199693320203 16.437438608579072 121.22370000000001 14.010400000000004 117.32739999999998 28.87999999999998 92.5977 26.09690000000002</gml:posList>
+                          </gml:LinearRing>
+                      </gml:exterior>
+                  </gml:Polygon>
+              </Value>
+          </Property>
+          <Filter>
+              <FeatureId fid="test.1"/>
+          </Filter>
+      </Update>
+    </Transaction>`;
+
+    const config: AxiosRequestConfig = {
+      headers: {
+        "Content-Type": "text/xml",
+        Authorization: `Basic ${ENCODE_BASIC_AUTH_GEOSERVER}`,
+      },
+    };
+
+    try {
+      const response = await axios.post(`${URL_GEOSERVER}/ows`, wfsTransaction, config);
+      if (response.status === 200) {
+        alert("Features saved successfully.");
+        setModifiedFeatures([]);
+        console.log(response.data);
+      } else {
+        console.error("Error saving features:", response);
+        alert("Error saving features.");
+      }
+    } catch (error) {
+      console.error("Error saving features:", error);
+      alert("Error saving features.");
+    }
+  };
+
+  return (
+    <>
+      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
+      <button onClick={handleOnClickSave} className="button-save-polygon">
+        Save
+      </button>
+    </>
+  );
 };
 
 export default MapPage;
